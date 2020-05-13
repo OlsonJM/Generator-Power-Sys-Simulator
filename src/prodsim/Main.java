a02275f2242e10bd7bede07bf172706441795304
James Olson <jamesmolson@eaton.com>
2020-05-13 10:19:22 -0500

    Add files via upload

---
+++ src/prodsim/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package prodsim;
+
+/**
+ *
+ * @author E0051024
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        MainApp runMe = new MainApp();
+        runMe.setVisible(true);
+    }
+
+}
